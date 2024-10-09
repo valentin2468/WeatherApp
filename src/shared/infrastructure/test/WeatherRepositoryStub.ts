59209846071c0da6504ea643bddf4fa393a61904
valentin2468 <valentinfyq@gmail.com>
2024-10-09 15:24:01 +0200

    feat: weather repository stub

--- src/shared/infrastructure/test/WeatherRepositoryStub.ts
+++ src/shared/infrastructure/test/WeatherRepositoryStub.ts
@@ -1,11 +1,13 @@
+import { Latitude } from "../../../Weather/domain/Latitude";
+import { Longitude } from "../../../Weather/domain/Longitude";
 import { Weather } from "../../../Weather/domain/Weather";
 import { WeatherRepository } from "../../../Weather/domain/WeatherRepository";
 
 export class WeatherRepositoryStub implements WeatherRepository {
   private toReturn: Weather[] = [];
   public async getWeatherByLatAndLng(
-    lat: number,
-    lng: number
+    lat: Latitude,
+    lng: Longitude
   ): Promise<Weather> {
     return this.toReturn[0];
   }
